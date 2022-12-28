3d742a0281d82f5293fa2280e2eb016f829cfa0c
szilardguti <szilardguti@gmail.com>
2022-12-28 21:49:00 +0100

    add base of ef core persistance

---
+++ Battleships/DAL/GameResultContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Battleships.DAL
+{
+    public class GameResultContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["BattleshipsDatabase"].ConnectionString);
+        }
+    }
+}
