bf49729e26297ba71c56b96a76bd535b54af8a88
CsonkaDavid <shay9505@gmail.com>
2022-12-29 00:37:14 +0100

    Made new base class for view-models to simplify property change handling

---
+++ Battleships/ViewModel/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using Battleships.ViewModel.Page;
+using System.ComponentModel;
+
+namespace Battleships.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static ViewModelBase Instance { get; set; }
+    }
+}
