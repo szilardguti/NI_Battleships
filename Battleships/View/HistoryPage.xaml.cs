1bfe258ca345b8547bbf76e6ac78b0df177aed39
CsonkaDavid <shay9505@gmail.com>
2022-12-28 17:34:52 +0100

    Created xaml files for all pages

---
+++ Battleships/View/HistoryPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Battleships.View
+{
+    /// <summary>
+    /// Interaction logic for HistoryPage.xaml
+    /// </summary>
+    public partial class HistoryPage : Page
+    {
+        public HistoryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Battleships/View/PlayerAIPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Battleships.View
+{
+    /// <summary>
+    /// Interaction logic for PlayerAIPage.xaml
+    /// </summary>
+    public partial class PlayerAIPage : Page
+    {
+        public PlayerAIPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
