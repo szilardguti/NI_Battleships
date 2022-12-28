08cc47a8e589809d04da7efaa3570b16a738c7f3
CsonkaDavid <shay9505@gmail.com>
2022-12-28 22:55:41 +0100

    Extracted navigation functions into a separate class with a singleton

--- Battleships/ViewModel/AppWindowViewModel.cs
+++ Battleships/ViewModel/AppWindowViewModel.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using Battleships.ViewModel.Navigation;
 using System.ComponentModel;
-using System.Linq;
 
 namespace Battleships.ViewModel
 {
@@ -16,31 +14,9 @@ namespace Battleships.ViewModel
             set { _currentPageViewModel = value; }
         }
 
-        private List<IPageViewModel> _pageViewModels;
-
-        public List<IPageViewModel> PageViewModels
-        {
-            get { return _pageViewModels; }
-            set { _pageViewModels = value; }
-        }
-
         public AppWindowViewModel()
         {
-            PageViewModels = new List<IPageViewModel>();
-
-            PageViewModels.Add(new MainPageViewModel());
-
-            CurrentPageViewModel = PageViewModels[0];
-        }
-
-        private void ChangeViewModel(IPageViewModel viewModel)
-        {
-            if (!PageViewModels.Contains(viewModel))
-            {
-                PageViewModels.Add(viewModel);
-            }
-
-            CurrentPageViewModel = PageViewModels.FirstOrDefault(vm => vm == viewModel);
+            CurrentPageViewModel = NavigationModule.Instance.CurrentPageViewModel;
         }
     }
 }
---
+++ Battleships/ViewModel/Navigation/NavigationModule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Battleships.ViewModel.Navigation
+{
+    public class NavigationModule
+    {
+        private static NavigationModule _instance;
+
+        public static NavigationModule Instance
+        {
+            get
+            {
+                _instance ??= new NavigationModule();
+
+                return _instance;
+            }
+        }
+
+        private IPageViewModel _currentPageViewModel;
+        public IPageViewModel CurrentPageViewModel
+        {
+            get { return _currentPageViewModel; }
+            set { _currentPageViewModel = value; }
+        }
+
+        private Collection<IPageViewModel> _pageViewModels;
+
+        public Collection<IPageViewModel> PageViewModels
+        {
+            get { return _pageViewModels; }
+            set { _pageViewModels = value; }
+        }
+
+        public NavigationModule()
+        {
+            PageViewModels = new Collection<IPageViewModel>
+            {
+                new MainPageViewModel()
+            };
+
+            CurrentPageViewModel = PageViewModels[0];
+        }
+
+        public void ChangeViewModel(IPageViewModel viewModel)
+        {
+            if (!PageViewModels.Contains(viewModel))
+            {
+                PageViewModels.Add(viewModel);
+            }
+
+            CurrentPageViewModel = PageViewModels.FirstOrDefault(vm => vm == viewModel);
+        }
+    }
+}
