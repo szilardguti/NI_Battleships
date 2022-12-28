2e0ca1b19671eaa0866884b32760fa318e9ca360
CsonkaDavid <shay9505@gmail.com>
2022-12-28 17:34:04 +0100

    Created common-property interface for view-models

---
+++ Battleships/ViewModel/IPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Battleships.ViewModel
+{
+    public interface IPageViewModel : INotifyPropertyChanged
+    {
+    }
+}
