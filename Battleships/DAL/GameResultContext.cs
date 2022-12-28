ebb08608e021f489bdbe75f8a1cf09fd02c9afc1
szilardguti <szilardguti@gmail.com>
2022-12-28 23:16:49 +0100

    add initial database migration

--- Battleships/DAL/GameResultContext.cs
+++ Battleships/DAL/GameResultContext.cs
@@ -1,17 +1,24 @@
-﻿using Battleships.DAL.Entities;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Text;
+using Battleships.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Battleships.DAL
 {
     public class GameResultContext : DbContext
     {
+        public DbSet<GameResult> Results { get; set; }
+        public DbSet<GameActions> Actions { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["BattleshipsDatabase"].ConnectionString);
+            // We use the exact string as using ConfigurationManager throws unexpected null reference error
+            // This seems to be a known bug fixed in EF Core 7.0
+            // To see more follow this thread: https://github.com/dotnet/efcore/issues/22205
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=battleships;Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ Battleships/Migrations/20221228215953_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Battleships.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstPlayerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondPlayerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstPlayerHitCount = table.Column<int>(type: "int", nullable: false),
+                    SecondPlayerHitCount = table.Column<int>(type: "int", nullable: false),
+                    RoundCount = table.Column<int>(type: "int", nullable: false),
+                    IsInProgress = table.Column<bool>(type: "bit", nullable: false),
+                    WinnerName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Actions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ActionString = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GameResultId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Actions_Results_GameResultId",
+                        column: x => x.GameResultId,
+                        principalTable: "Results",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actions_GameResultId",
+                table: "Actions",
+                column: "GameResultId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actions");
+
+            migrationBuilder.DropTable(
+                name: "Results");
+        }
+    }
+}
