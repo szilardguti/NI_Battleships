b230e15016117d064fc11c9642903c54e6c0ef6d
szilardguti <szilardguti@gmail.com>
2022-12-28 19:31:24 +0100

    add global suppression so NI namespace will not be enforced

---
+++ Battleships/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Correctness", "LRT001:There is only one restricted namespace", Justification = "NI namespace should not be forced", Scope = "namespaceanddescendants", Target = "~N:Battleships")]
